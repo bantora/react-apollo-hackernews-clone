31cc628d6ca48826ea9d5a9fec45b8c68041d680
Ryan Paolo Joson <bantora.1011@gmail.com>
2024-04-02 21:30:09 +0800

    feat: filtering, sorting and pagination

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,8 +1,25 @@
 /* eslint-disable */
-function feed(parent, args, context) {
-  return context.prisma.link.findMany();
+async function feed(_parent, { filter, skip, take, orderBy }, context) {
+  const where = filter
+    ? {
+        OR: [
+          { description: { contains: filter } },
+          { url: { contains: filter } },
+        ],
+      }
+    : {};
+  const links = await context.prisma.link.findMany({
+    where,
+    skip,
+    take,
+    orderBy,
+  });
+
+  const count = await context.prisma.link.count({ where });
+
+  return { links, count };
 }
 
 module.exports = {
-	feed
-}
+  feed,
+};
