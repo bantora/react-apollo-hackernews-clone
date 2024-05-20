ca08d89d3502aa3934a885506158c5f0a50de5a3
Ryan Paolo Joson <bantora.1011@gmail.com>
2024-05-20 21:20:35 +0800

    chore: getting started setup for react + apollo

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './components/App.tsx'
+import './styles/index.css'
 import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 
 const httpLink = createHttpLink({
